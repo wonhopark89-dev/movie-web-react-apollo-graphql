774436ed33051044da52aa16fd32433bc93129ee
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-04-06 23:55:37 +0900

    add sample query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,23 @@
 import React from "react";
-export default () => <h1>home</h1>;
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
+};
