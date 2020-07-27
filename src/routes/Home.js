0ec02511a6e751fbd3207254345ce515bb6f5827
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-07-27 23:56:28 +0900

    add styled component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -48,6 +48,15 @@ const Loading = styled.div`
   margin-top: 10px;
 `;
 
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
 export default () => {
   const { loading, error, data } = useQuery(GET_MOVIES);
   console.log(loading, error, data);
@@ -58,9 +67,13 @@ export default () => {
         <Subtitle>I love GraphQL</Subtitle>
       </Header>
       {loading && <Loading>Loading...</Loading>}
-      {!loading &&
-        data.movies &&
-        data.movies.map((m) => <Movie key={m.id} id={m.id} />)}
+      {!loading && data.movies && (
+        <Movies>
+          {data.movies.map((m) => (
+            <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+          ))}
+        </Movies>
+      )}
     </Container>
   );
 };
