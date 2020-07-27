e262dd8a0735f7e241a2745f6be9000bf8a2dfac
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-07-27 23:26:40 +0900

    add graphql with option, other useful hooks

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "https://movieql.now.sh/"
+  uri: "https://movieql2.now.sh/"
 });
 
 export default client;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,2 +1,25 @@
 import React from "react";
-export default () => <h2>Detail</h2>;
+import { useParams } from "react-router-dom"; // paramter 가져오는 Hook
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIE = gql`
+    query getMovie($id: Int!) {
+        movie(id: $id) {
+            id
+            title
+            medium_cover_image
+            description_intro 
+        }
+    }
+`;
+
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id }, // { id : id }
+  });
+  if (loading) return "loading";
+
+  if (data && data.movie) return data.movie.title;
+};
