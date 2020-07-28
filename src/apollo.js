d4d4263e7da47655ba080f9aff9b03a21debf4c3
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-07-28 23:54:42 +0900

    add Local State with resolvers in Apollo Config

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,14 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "https://movieql2.now.sh/"
+  uri: "https://movieql2.now.sh/",
+  // make isLiked from client
+  // uri: "http://localhost:4000",
+  resolvers: {
+    Movie: {
+      isLiked: () => false
+    }
+  }
 });
 
 export default client;
