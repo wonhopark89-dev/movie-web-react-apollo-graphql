7bbd02ebe0f492954ef6b2dcba5fd95cfc3b077e
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-07-18 00:57:44 +0900

    graphql scalar and root type defs

--- 2022/tweetql-server/server.js
+++ 2022/tweetql-server/server.js
@@ -1,6 +1,24 @@
 import { ApolloError, ApolloServer, gql } from 'apollo-server';
 
-const server = new ApolloServer({});
+const typeDefs = gql`
+  type User {
+    id: ID
+    username: String
+  }
+
+  type Tweet {
+    id: ID
+    text: String
+    author: User
+  }
+
+  type Query {
+    allTweets: [Tweet]
+    tweet(id: ID): Tweet
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
 
 server.listen().then(({ url }) => {
   console.log(url);
