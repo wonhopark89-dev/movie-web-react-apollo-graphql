1292275c3748827d3eaca25ca0555d08a3c1b6d7
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-07-19 00:57:34 +0900

    nullable or non-nullable

--- 2022/tweetql-server/server.js
+++ 2022/tweetql-server/server.js
@@ -7,19 +7,19 @@ const typeDefs = gql`
   }
 
   type Tweet {
-    id: ID
-    text: String
-    author: User
+    id: ID!
+    text: String!
+    author: User!
   }
 
   type Query {
     allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    tweet(id: ID!): Tweet
   }
 
   type Mutation {
     postTweet(text: String, userId: ID): Tweet
-    deleteTweet(id: ID): Boolean
+    deleteTweet(id: ID!): Boolean!
   }
 `;
 
